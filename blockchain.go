8fbfb2d86048122bc6290a3deaa99b55baf7a254
ray <mathew177@gmail.com>
2019-02-07 16:54:04 +0800

    Blockchain

---
+++ blockchain.go
@@ -0,0 +1,20 @@
+package main
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+// NewGenesisBlock - Init first block
+func NewGenesisBlock() *Block {
+	return NewBlock("Genesis Block", []byte{})
+}
+
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
